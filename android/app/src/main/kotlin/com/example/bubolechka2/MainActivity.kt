ee78ae2629ab5c6ec49406fc26c3b1deda0e92ff
LRGeorgiev <lrgeorgiev20@codingburgas.bg>
2023-07-04 22:18:03 +0300

    Chnage the structure and theme of the program

--- android/app/src/main/kotlin/com/example/bubolechka2/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.bubolechka2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,224 +1,44 @@
-import 'package:bubolechka2/data/categories.dart';
-import 'package:bubolechka2/language_selector.dart';
-import 'package:bubolechka2/models/bubo_category.dart';
 import 'package:flutter/material.dart';
+import 'package:shop_ui_with_ai/constants.dart';
+import 'package:shop_ui_with_ai/screens/splash/splash_screen.dart';
 
 void main() {
-  runApp(const BuboApp());
+  runApp(const MyApp());
 }
 
-class BuboApp extends StatelessWidget {
-  const BuboApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bubolechka 2',
+      title: 'The Flutter Way',
       theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
         primarySwatch: Colors.blue,
-      ),
-      home: const BuboHomePage(),
-    );
-  }
-}
-
-///
-/// HomePage of the Bubolechka Application
-///
-/// Displays the available categories and language selection on the main screen.
-///
-class BuboHomePage extends StatefulWidget {
-  const BuboHomePage({super.key});
-
-  @override
-  State<BuboHomePage> createState() => _BuboHomePageState();
-}
-
-class _BuboHomePageState extends State<BuboHomePage> {
-  String _language = 'bg';
-
-  @override
-  Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
-    var height = MediaQuery.of(context).size.height;
-
-    return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            constraints: const BoxConstraints.expand(),
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/main_background.png'),
-                    fit: BoxFit.cover)),
-            child: null,
-          ),
-          Positioned(
-            top: 30,
-            bottom: 100,
-            left: 20,
-            right: 20,
-            child: BuboCategoryViewer(_language),
-          ),
-          Positioned(
-              right: 30,
-              bottom: 30,
-              width: width > height ? 300 : 54,
-              height: width < height ? 300 : 54,
-              child: LanguageSelector(width > height,
-                  onLanguageChange: (newLanguage) {
-                setState(() {
-                  _language = newLanguage.toLowerCase();
-                });
-              })),
-          Positioned(
-            bottom: 30,
-            left: 30,
-            width: 200,
-            child: Image.asset('assets/bubo_logo.png'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-///
-/// Category view for the main screen
-///
-/// Gets all available categories and places them into a list view
-///
-///
-class BuboCategoryViewer extends StatelessWidget {
-  final String language;
-  BuboCategoryViewer(this.language, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
-    var height = MediaQuery.of(context).size.height;
-
-    var widthPerCategory = 300;
-    //print('Device size is: $width x $height');
-    if (width > widthPerCategory) {
-      var itemsPerRow = (width / widthPerCategory).floor();
-      var rows = (buboCategories.length / itemsPerRow).ceil();
-      //print('items per row: $itemsPerRow, rows: $rows');
-
-      return GridView.count(
-        mainAxisSpacing: 10,
-        crossAxisSpacing: 0,
-        crossAxisCount: itemsPerRow,
-        children: buboCategories.map((buboCategory) {
-          return BuboCategoryListItem(language, buboCategory);
-        }).toList(),
-      );
-
-
-
-      
-
-
-
-
-
-
-    } else {
-      return ListView(
-        children: buboCategories.map((buboCategory) {
-          return BuboCategoryListItem(language, buboCategory);
-        }).toList(),
-      );
-    }
-
-    // return ListView(
-    //   children: buboCategories.map((buboCategory) {
-    //     return BuboCategoryListItem(language, buboCategory);
-    //   }).toList(),
-    // );
-  }
-}
-
-///
-/// Widget for displaying a category on the main screen
-///
-/// TODO: Модифицирайте този клас така, че да показва не само името на
-///       категорията, но и нейната картинка.
-///       Картинките за всяка категория могат да се вземат по следният начин:
-///       Image.asset(category.image)
-///
-///
-class BuboCategoryListItem extends StatelessWidget {
-  final String language;
-  final BuboCategory category;
-
-  const BuboCategoryListItem(this.language, this.category, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: [
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(category.image, width: 250, fit: BoxFit.fill),
-          RotatedBox(
-            quarterTurns: 1,
-            child: Text(
-              category.translatedLabels[language]!,
-              style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30,
-                  color: Colors.white),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            minimumSize: const Size(double.infinity, 48),
+            backgroundColor: const Color(0xFFF2994A),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(14),
+              side: BorderSide.none,
             ),
-          )
-        ],
+            elevation: 0,
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          fillColor: const Color(0xFFFBFBFB),
+          filled: true,
+          border: defaultOutlineInputBorder,
+          enabledBorder: defaultOutlineInputBorder,
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(14),
+            borderSide: const BorderSide(color: Color(0xFFF2994A)),
+          ),
+        ),
       ),
-      Container(
-        height: 10,
-      )
-      //_viewWithContainer(),
-      //_viewWithStack(),
-    ]);
-  }
-
-  Widget _viewWithStack() {
-    return Stack(
-      children: [
-        SizedBox(width: 250, height: 250, child: Image.asset(category.image)),
-        Positioned(
-            top: 190,
-            left: 0,
-            width: 250,
-            child: Center(
-                child: Text(
-              category.translatedLabels[language] != null
-                  ? category.translatedLabels[language]!
-                  : 'Not available',
-              style: const TextStyle(color: Colors.white, fontSize: 30),
-            ))),
-      ],
+      home: const SplashScreen(),
     );
   }
-
-  Widget _viewWithContainer() {
-    return Container(
-        width: 250,
-        height: 250,
-        decoration: BoxDecoration(
-            image: DecorationImage(image: AssetImage(category.image))),
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 150, 0, 0),
-          child: Center(
-            child: Text(
-              category.translatedLabels[language] != null
-                  ? category.translatedLabels[language]!
-                  : 'Not available',
-              style: const TextStyle(color: Colors.white, fontSize: 30),
-            ),
-          ),
-        ));
-  }
 }
-
-
